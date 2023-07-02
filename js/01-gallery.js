4390b534a393c3dbb0293e729ac1d99a8da8e51e
Armash11 <124906592+Armash11@users.noreply.github.com>
2023-07-02 16:51:15 +0200

    task-01

---
+++ js/01-gallery.js
@@ -0,0 +1,37 @@
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector(".gallery");
+const cardsMarkup = galleryItems
+  .map(
+    ({ preview, original }) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="Image description"
+    />
+  </a>
+  </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", cardsMarkup);
+gallery.addEventListener("click", onShowImege);
+
+function onShowImege(evt) {
+  evt.preventDefault();
+
+  const isGalleryImage = evt.target.classList.contains("gallery__image");
+
+  if (!isGalleryImage) {
+    return;
+  }
+  const currentItem = evt.target.getAttribute("data-source");
+  const instance = basicLightbox.create(`
+    <img class="gallery__image" src="${currentItem}" alt="Image description" width="800" height="600">
+`);
+
+  instance.show();
+}
