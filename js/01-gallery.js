ff6e201f3dc61fa77597e2f7e2124d97e34bc474
Armash11 <124906592+Armash11@users.noreply.github.com>
2023-07-03 16:49:16 +0200

    task-02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,14 +3,14 @@ import { galleryItems } from "./gallery-items.js";
 const gallery = document.querySelector(".gallery");
 const cardsMarkup = galleryItems
   .map(
-    ({ preview, original }) => `
+    ({ preview, original, description }) => `
     <li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
-      alt="Image description"
+      alt="${description}"
     />
   </a>
   </li>`
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const gallery = document.querySelector(".gallery");
+const cardsMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML("beforeend", cardsMarkup);
+gallery.addEventListener("click", onShowImege);
+
+function onShowImege(evt) {
+  evt.preventDefault();
+  const isGalleryImage = evt.target.classList.contains("gallery__image");
+
+  if (!isGalleryImage) {
+    return;
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    animationSpeed: 250,
+  });
+});
